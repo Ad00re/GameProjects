93086168dc6a352b70cc989271adb3ed6e2064ba
Christina Wang <christinawang521@gmail.com>
2024-04-04 16:49:48 -0400

    init mineSweeper game, add start/game/end scene

---
+++ MineSweeper/Assets/Scripts/Box.cs
@@ -0,0 +1,142 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Box : MonoBehaviour
+{
+    
+    public Sprite flag;
+    public bool mine;
+    public int neighbour;
+    public int scale;
+    public Sprite[] number;
+    public float delayTime = 1f;
+    public float time = 0f;
+    public bool die;
+    //three states, 0 for init state, 1 for flag, 2 for opened
+    public int state;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (die)
+        {
+            time += Time.deltaTime;
+        }
+
+        if (time >= delayTime)
+        {
+            SceneManager.LoadScene("Scenes/End");
+        }
+    }
+    public void ClickOn()
+    {
+        GameObject self = GameObject.Find(name);
+        if (state == 0)
+        {
+            if (Input.GetMouseButtonUp(1))
+            {
+                ChangeSpriteFlag(self);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (mine)
+                {
+                    ChangeSpriteMine(self);
+                    die = true;
+                }
+                else
+                {
+                    ChangeSprite(self);
+                    // first find the x and y
+                    (int x, int y) = findLocation(GameObject.Find(name));
+                    // bfs to expand neighbour
+                    List<(int, int)> queue = new List<(int, int)>();
+                    if (neighbour == 0)
+                    {
+                        queue.Add((x,y));
+                    }
+                    while (queue.Count > 0)
+                    {
+                        ( x,  y)= queue[0];
+                        queue.RemoveAt(0);
+                        // get the valid neighbours
+                        GameObject curBox;
+                        for (int i = -1; i < 2; i++)
+                        {
+                            if (x + i < 0 || x + i >= scale)
+                            {
+                                continue;
+                            }
+                            for (int j = -1; j < 2; j++)
+                            {
+                                if (y + j >= 0 && y + j < scale)
+                                {
+                                    if (i == 0 && j == 0)
+                                    {
+                                        continue;
+                                    }
+                                    curBox = GameObject.Find("box" + (x+i) + "_" + (y+j));
+                                    int stateBeforeModify = curBox.GetComponent<Box>().state;
+                                    ChangeSprite(curBox);
+                                    if (curBox.GetComponent<Box>().neighbour==0 && stateBeforeModify==0)
+                                    {
+                                        queue.Add((x+i,y+j));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+
+                    
+                }
+
+                (int,int) findLocation(GameObject o)
+                {
+                    
+                    Regex pattern = new Regex(@"box(\d+)_(\d+)");
+                    Match match = pattern.Match(o.name);
+                    int x = int.Parse(match.Groups[1].Value);  
+                    int y = int.Parse(match.Groups[2].Value);
+                    return (x, y);
+                }
+
+                
+            }
+            
+            void ChangeSpriteFlag(GameObject o)
+            {
+                o.GetComponent<SpriteRenderer>().sprite = flag;
+                o.GetComponent<SpriteRenderer>().color = Color.white;
+                o.transform.localScale = new Vector3((1024f / scale) / flag.rect.size.x,
+                    (1024f / scale) / flag.rect.size.y, 1f);
+                o.GetComponent<Box>().state = 1;
+            }
+                
+            void ChangeSpriteMine(GameObject o)
+            {
+                o.GetComponent<SpriteRenderer>().sprite = flag;
+                o.GetComponent<SpriteRenderer>().color = Color.red;
+                o.transform.localScale = new Vector3((1024f / scale) / flag.rect.size.x,
+                    (1024f / scale) / flag.rect.size.y, 1f);
+                o.GetComponent<Box>().state = 2;
+            }
+                
+            void ChangeSprite(GameObject o)
+            {
+                Sprite image = number[o.GetComponent<Box>().neighbour];
+                o.GetComponent<SpriteRenderer>().sprite = image;
+                o.GetComponent<SpriteRenderer>().color = Color.white;
+                o.transform.localScale = new Vector3((1024f / scale) / image.rect.size.x,
+                    (1024f / scale) / image.rect.size.y, 1f);
+                o.GetComponent<Box>().state = 2;
+            }
+
+        }
+
+    }
+}
---
+++ MineSweeper/Assets/Scripts/MineManager.cs
@@ -0,0 +1,99 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MineManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int gridSize;
+    public int mineCount;
+    private float windowSize =10f;
+    public bool[,] grid;
+    public List<Vector2> grid2;
+    
+    [SerializeField] public GameObject[] boxs;
+    
+    
+    
+    
+    
+    void Start()
+    {
+        if (StateManager.Difficulty == "Easy")
+        {
+            gridSize = 9;
+            mineCount = 10;
+        }
+        else if (StateManager.Difficulty == "Medium")
+        {
+            gridSize = 16;
+            mineCount = 40;
+        }
+        else if (StateManager.Difficulty == "Hard")
+        {
+            gridSize = 25;
+            mineCount = 99;
+        }
+        
+        grid = new bool[gridSize, gridSize];
+        for (int i = 0; i < mineCount; i++)
+        {
+            int x;
+            int y;
+            do
+            {
+                x = Random.Range(0, gridSize-1);
+                y = Random.Range(0, gridSize-1);
+            } while (grid[x, y]);
+            Debug.Log(x+","+y);
+            grid[x, y] = true;
+            
+        }
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                GameObject newBox = Instantiate(boxs[(i+j)%2]);
+                // 总共十个格子，n*n的时候每个格子大小是10/（n+1）
+                // 那么 i就在（i-gridSize/2f+0.5f） * 10 /（n+1）
+                newBox.transform.position = new Vector3((i-gridSize/2f+0.5f) *windowSize /(gridSize+1), (j-gridSize/2f+0.5f) *windowSize /(gridSize+1), 0);
+                newBox.transform.localScale = new Vector3(windowSize /(gridSize+1), windowSize /(gridSize+1), 0);
+                newBox.name = "box" + i.ToString() + "_" + j.ToString();
+                newBox.GetComponent<Box>().mine = grid[i, j];
+                newBox.GetComponent<Box>().scale = gridSize;
+                newBox.GetComponent<Box>().neighbour = checkNeighbor(new Vector2Int(i, j));
+
+            }
+        }
+    }
+
+    public int checkNeighbor(Vector2Int loc)
+    {
+        int counter = 0;
+        for (int i = -1; i < 2; i++)
+        {
+            if (loc.x + i < 0 || loc.x + i >= gridSize)
+            {
+                continue;
+            }
+            for (int j = -1; j < 2; j++)
+            {
+                if (loc.y + j >= 0 && loc.y + j < gridSize)
+                {
+                    if (grid[loc.x + i, loc.y + j])
+                    {
+                        counter += 1;
+                    }
+                }
+            }
+        }
+        return counter;
+    } 
+    
+    
+    
+    
+}
