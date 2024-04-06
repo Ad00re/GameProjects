8a0a8bfbe96f0641a468789a77f4b8558737b9ae
Christina Wang <christinawang521@gmail.com>
2024-04-06 01:28:46 -0400

    fixed coding style

--- Assets/Scripts/MineManager.cs
+++ Assets/Scripts/MineManager.cs
@@ -1,20 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MineManager : MonoBehaviour
 {
-    public static int  gridSize;
-    public static int mineCount;
-    public static int[,] grid;
-    public static int[,] states;
+    public enum State 
+    {
+        Init,
+        Flagged,
+        Opened
+    }
+    public int  gridSize;
+    public int mineCount;
+    public int[,] gameGrid;
+    public State[,] gridStates;
+    
+    public static MineManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
     
     void Start()
     {
         gridSize = 9;
         mineCount = 10;
-        grid = new int[gridSize, gridSize];
-        states = new int[gridSize, gridSize];
+        gameGrid = new int[gridSize, gridSize];
+        gridStates = new State[gridSize, gridSize];
     }
 
     // Update is called once per frame
