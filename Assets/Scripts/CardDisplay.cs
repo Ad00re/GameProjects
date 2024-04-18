8806be7dd34886224614a355b637f48ef05b41a2
Christina Wang <christinawang521@gmail.com>
2024-04-18 15:22:39 -0400

    Created CardDisplay, Able to initialzied card on screen

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public Text rankText;
+    [SerializeField] public Text suitText;
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+}
