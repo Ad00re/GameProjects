f39f1b390ded00ee06aff87eff0058da8de914c7
Christina Wang <christinawang521@gmail.com>
2024-04-18 15:22:39 -0400

    Created CardDisplay, Able to initialzied card on screen

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 public enum Suit
 {
     Clubs, Diamonds, Hearts, Spades
@@ -21,21 +23,25 @@ public class Card
         Suit = suit;
         Rank = rank;
     }
-
-    public override string ToString()
-    {
-        return $"{Rank} of {Suit}";
-    }
 }
 
 public class CardManager : MonoBehaviour
 {
     public List<Card> Cards;
+    public List<int> Deck;
+    public List<int> prevDrawed;
+    public List<int> Drawed;
+    public List<int> Selected;
+    private bool modified = false;
+
+    [SerializeField] private GameObject cardPrefab;
     
     // Start is called before the first frame update
     void Start()
     {
         Cards = new List<Card>();
+        Deck = new List<int>();
+        Drawed = new List<int>();
         foreach (Suit suit in System.Enum.GetValues(typeof(Suit)))
         {
             foreach (Rank rank in System.Enum.GetValues(typeof(Rank)))
@@ -44,12 +50,57 @@ public class CardManager : MonoBehaviour
             }
         }
 
+        for (int i = 0; i < Cards.Count; i++)
+        {
+            Deck.Add(i);
+        }
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (modified)
+        {
+            UpdateDrawDisplay();
+        }
         
+    }
+
+    public void DrawCard()
+    {
+        Debug.Log(Deck.Count);
+        Debug.Log(Drawed.Count);
+        int numberOfDraw = 7 - Drawed.Count;
+        for (int i = 0; i < numberOfDraw; i++)
+        {
+            int indexOfCard = Random.Range(0, Deck.Count);
+            Drawed.Add(Deck[indexOfCard]);
+            Deck.RemoveAt(indexOfCard);
+            modified = true;
+        }
+        
+    }
+
+    void UpdateDrawDisplay()
+    {
+        int counter = -1;
+        foreach (int ind in Drawed)
+        {
+            if (!prevDrawed.Contains(ind))
+            {
+                GameObject card = Instantiate(cardPrefab);
+                card.transform.Translate(counter,0,0);
+                card.GetComponent<CardDisplay>().rankText.text = Cards[ind].Rank.ToString();
+                card.GetComponent<CardDisplay>().suitText.text = Cards[ind].Suit.ToString();
+                card.SetActive(true);
+                counter += 1;
+            }
+        }
+
+        modified = false;
+
     }
 }
 
